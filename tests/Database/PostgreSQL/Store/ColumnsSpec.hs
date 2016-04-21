90161fa97d6c7d914a68ab9874e29bb7d6cb8ac1
Ole <ole@vprsm.de>
2016-04-21 15:37:11 +0200

    tests: Rewrite test and doc comments for ColumnSpec

--- tests/Database/PostgreSQL/Store/ColumnsSpec.hs
+++ tests/Database/PostgreSQL/Store/ColumnsSpec.hs
@@ -12,105 +12,110 @@ import           Test.Hspec
 
 import           Database.PostgreSQL.Store.Columns
 
-testColumnHomomorphism :: (Column a, Show a, Eq a) => a -> Expectation
-testColumnHomomorphism val =
+-- | Pack and unpack value to test if the input value is the same as the output value.
+testColumnIsomorphism :: (Column a, Show a, Eq a) => a -> Expectation
+testColumnIsomorphism val =
 	shouldBe (unpack (pack val)) (Just val)
 
-propColumnHomomorphism :: (Column a, Arbitrary a, Eq a, Show a) => Proxy a -> Property
-propColumnHomomorphism proxy =
+-- | Quick check an instance of Column to see if it behaves isomorphic.
+propColumnIsomorphism :: (Column a, Arbitrary a, Eq a, Show a) => Proxy a -> Property
+propColumnIsomorphism proxy =
 	property (compare proxy)
 	where
 		compare :: (Column a, Arbitrary a, Eq a, Show a) => Proxy a -> a -> Bool
 		compare _ x =
 			unpack (pack x) == Just x
 
-propColumnHomomorphism' :: (Arbitrary b, Show b, Column a, Eq a, Show a) => ([b] -> a) -> Property
-propColumnHomomorphism' make =
+-- | Quick check an instance of Column which has to be generated first, because it does not have an
+--   instance of Arbitrary.
+propColumnIsomorphism' :: (Arbitrary b, Show b, Column a, Eq a, Show a) => ([b] -> a) -> Property
+propColumnIsomorphism' make =
 	property (\ xs -> unpack (pack (make xs)) == Just (make xs))
 
+-- | Test for instances of Column
 columnsSpec :: Spec
 columnsSpec = do
 	describe "instance Column Bool" $
-		it "homomorphic" $ do
-			testColumnHomomorphism True
-			testColumnHomomorphism False
+		it "must behave isomorphic" $ do
+			testColumnIsomorphism True
+			testColumnIsomorphism False
 
 	describe "instance Column Int" $ do
-		it "homomorphic" $ do
-			testColumnHomomorphism (minBound :: Int)
-			testColumnHomomorphism (0 :: Int)
-			testColumnHomomorphism (maxBound :: Int)
+		it "must behave isomorphic" $ do
+			testColumnIsomorphism (minBound :: Int)
+			testColumnIsomorphism (0 :: Int)
+			testColumnIsomorphism (maxBound :: Int)
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism (Proxy :: Proxy Int)
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism (Proxy :: Proxy Int)
 
 	describe "instance Column Int8" $ do
-		it "homomorphic" $ do
-			testColumnHomomorphism (minBound :: Int8)
-			testColumnHomomorphism (0 :: Int8)
-			testColumnHomomorphism (maxBound :: Int8)
+		it "must behave isomorphic" $ do
+			testColumnIsomorphism (minBound :: Int8)
+			testColumnIsomorphism (0 :: Int8)
+			testColumnIsomorphism (maxBound :: Int8)
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism (Proxy :: Proxy Int8)
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism (Proxy :: Proxy Int8)
 
 	describe "instance Column Int16" $ do
-		it "homomorphic" $ do
-			testColumnHomomorphism (minBound :: Int16)
-			testColumnHomomorphism (0 :: Int16)
-			testColumnHomomorphism (maxBound :: Int16)
+		it "must behave isomorphic" $ do
+			testColumnIsomorphism (minBound :: Int16)
+			testColumnIsomorphism (0 :: Int16)
+			testColumnIsomorphism (maxBound :: Int16)
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism (Proxy :: Proxy Int16)
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism (Proxy :: Proxy Int16)
 
 	describe "instance Column Int32" $ do
-		it "homomorphic" $ do
-			testColumnHomomorphism (minBound :: Int32)
-			testColumnHomomorphism (0 :: Int32)
-			testColumnHomomorphism (maxBound :: Int32)
+		it "must behave isomorphic" $ do
+			testColumnIsomorphism (minBound :: Int32)
+			testColumnIsomorphism (0 :: Int32)
+			testColumnIsomorphism (maxBound :: Int32)
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism (Proxy :: Proxy Int32)
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism (Proxy :: Proxy Int32)
 
 	describe "instance Column Int64" $ do
-		it "homomorphic" $ do
-			testColumnHomomorphism (minBound :: Int64)
-			testColumnHomomorphism (0 :: Int64)
-			testColumnHomomorphism (maxBound :: Int64)
+		it "must behave isomorphic" $ do
+			testColumnIsomorphism (minBound :: Int64)
+			testColumnIsomorphism (0 :: Int64)
+			testColumnIsomorphism (maxBound :: Int64)
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism (Proxy :: Proxy Int64)
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism (Proxy :: Proxy Int64)
 
 	describe "instance Column [Char]" $ do
-		it "homomorphic" $
-			testColumnHomomorphism ([] :: [Char])
+		it "must behave isomorphic" $
+			testColumnIsomorphism ([] :: [Char])
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism (Proxy :: Proxy [Char])
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism (Proxy :: Proxy [Char])
 
 	describe "instance Column B.ByteString" $ do
-		it "homomorphic" $
-			testColumnHomomorphism B.empty
+		it "must behave isomorphic" $
+			testColumnIsomorphism B.empty
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism' B.pack
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism' B.pack
 
 	describe "instance Column BL.ByteString" $ do
-		it "homomorphic" $
-			testColumnHomomorphism BL.empty
+		it "must behave isomorphic" $
+			testColumnIsomorphism BL.empty
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism' BL.pack
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism' BL.pack
 
 	describe "instance Column T.Text" $ do
-		it "homomorphic" $
-			testColumnHomomorphism T.empty
+		it "must behave isomorphic" $
+			testColumnIsomorphism T.empty
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism' T.pack
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism' T.pack
 
 	describe "instance Column TL.Text" $ do
-		it "homomorphic" $
-			testColumnHomomorphism TL.empty
+		it "must behave isomorphic" $
+			testColumnIsomorphism TL.empty
 
-		it "homomorphic (qc)" $
-			propColumnHomomorphism' TL.pack
+		it "must behave isomorphic (using QuickCheck)" $
+			propColumnIsomorphism' TL.pack
