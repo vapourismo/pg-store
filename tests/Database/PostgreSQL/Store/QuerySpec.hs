9c35b63fe3ef10ca4db937888059caa95459bab5
Ole <ole@vprsm.de>
2016-06-16 01:16:09 +0200

    tests: Add new test for 'pgsq'

--- tests/Database/PostgreSQL/Store/QuerySpec.hs
+++ tests/Database/PostgreSQL/Store/QuerySpec.hs
@@ -11,8 +11,9 @@ import Database.PostgreSQL.Store
 import Database.PostgreSQL.Store.Query
 import Database.PostgreSQL.Store.Columns
 
-data MyType = MyConstructor Int
-	deriving (Show)
+data MyType = MyConstructor {
+	myField :: Int
+} deriving (Show)
 
 instance DescribableTable MyType where
 	describeTableName _ = "InsertTableName"
@@ -34,3 +35,7 @@ querySpec =
 
 		it "must insert and pack variables correctly" $
 			[pgsq|$testValue|] `shouldBe` Query "$1" [pack testValue]
+
+		it "must resolve fields correctly" $
+			queryStatement [pgsq|myField|]
+				`shouldBe` fromString ("\"Database.PostgreSQL.Store.QuerySpec.myField\"")
