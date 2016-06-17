2b1a2c8beedb2ecc07ef6365af2c88f7b8216eee
Ole <ole@vprsm.de>
2016-06-17 16:46:01 +0200

    Simplify result processing

--- src/Database/PostgreSQL/Store/Result.hs
+++ src/Database/PostgreSQL/Store/Result.hs
@@ -1,192 +1,103 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 -- |
 -- Module:     Database.PostgreSQL.Store.Result
 -- Copyright:  (c) Ole Krüger 2015-2016
 -- License:    BSD3
 -- Maintainer: Ole Krüger <ole@vprsm.de>
 module Database.PostgreSQL.Store.Result (
-	-- *
-	ResultError (..),
 	ResultProcessor,
-	ColumnInfo,
-	runResultProcessor,
-	columnNumber,
-	columnType,
-	columnFormat,
-	columnInfo,
-	foreachRow,
-	cellValue,
-	unpackCellValue,
-
-	-- * Result
-	Result (..),
-	RawRow (..)
+	ResultError (..),
+	unpackColumn,
+	processResult,
+	processOneResult
 ) where
 
 import           Control.Monad
 import           Control.Monad.Trans.Class
+import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Reader
+import           Control.Monad.Trans.State.Strict
 import           Control.Monad.Trans.Except
 
-import           Data.List
-import           Data.Typeable
-import qualified Data.ByteString           as B
+import           Data.Proxy
 
 import qualified Database.PostgreSQL.LibPQ as P
 import           Database.PostgreSQL.Store.Columns
 
 -- | Error that occured during result processing
 data ResultError
-	= ColumnMissing B.ByteString
-	| ValueError P.Row P.Column P.Oid P.Format ColumnDescription
+	= TooFewColumnsError P.Column
+	  -- ^ Occurs when you're trying to access a column that does not exist.
+	| UnpackError P.Row P.Column P.Oid P.Format ColumnDescription
+	  -- ^ The value at a given row and column could not be unpacked.
 	deriving (Show, Eq)
 
 -- | Result processor
-type ResultProcessor = ReaderT P.Result (ExceptT ResultError IO)
+newtype ResultProcessor a =
+	ResultProcessor (StateT P.Column (ReaderT (P.Result, P.Row, P.Column) (ExceptT ResultError IO)) a)
+	deriving (Functor, Applicative, Monad, MonadIO)
 
--- | Column information
-type ColumnInfo = (P.Column, P.Oid, P.Format)
+-- | Read information packed into the "ReaderT".
+readInfo :: ResultProcessor (P.Result, P.Row, P.Column)
+readInfo = ResultProcessor (lift ask)
 
--- | Process the result.
-runResultProcessor :: P.Result -> ResultProcessor a -> ExceptT ResultError IO a
-runResultProcessor = flip runReaderT
+-- | Get the current column.
+getColumn :: ResultProcessor P.Column
+getColumn = ResultProcessor get
 
--- | Get the number of columns.
-numColumns :: ResultProcessor P.Column
-numColumns = do
-	result <- ask
-	lift (lift (P.nfields result))
+-- | Set the new column.
+setColumn :: P.Column -> ResultProcessor ()
+setColumn col = ResultProcessor (put col)
 
--- | Get the column number for a column name.
-columnNumber :: B.ByteString -> ResultProcessor P.Column
-columnNumber name = do
-	result <- ask
-	lift (ExceptT (maybe (Left (ColumnMissing name)) pure <$> P.fnumber result name))
+-- | Throw a nerror.
+throwError :: ResultError -> ResultProcessor a
+throwError err = ResultProcessor (lift (lift (throwE err)))
 
--- | Get the type of a column.
-columnType :: P.Column -> ResultProcessor P.Oid
-columnType col = do
-	result <- ask
-	lift (lift (P.ftype result col))
+-- | Unpack a column.
+unpackColumn :: (Column a) => ResultProcessor a
+unpackColumn = do
+	-- Gather information
+	col <- getColumn
+	(res, row, numCol) <- readInfo
 
--- | Get the format of a column.
-columnFormat :: P.Column -> ResultProcessor P.Format
-columnFormat col = do
-	result <- ask
-	lift (lift (P.fformat result col))
+	-- Make sure we're not trying to unpack a non-existing column
+	when (col >= numCol) (throwError (TooFewColumnsError numCol))
 
--- | Get information about a column.
-columnInfo :: B.ByteString -> ResultProcessor ColumnInfo
-columnInfo name = do
-	col <- columnNumber name
-	(,,) col <$> columnType col <*> columnFormat col
+	-- Retrieve column-specific information
+	(typ, fmt, mbData) <- ResultProcessor . liftIO $
+		(,,) <$> P.ftype res col
+		     <*> P.fformat res col
+		     <*> P.getvalue' res row col
 
--- | Do something for each row.
-foreachRow :: (P.Row -> ResultProcessor a) -> ResultProcessor [a]
-foreachRow rowProcessor = do
-	result <- ask
-	numRows <- lift (lift (P.ntuples result))
-	mapM rowProcessor [0 .. numRows - 1]
+	-- Try to unpack the value
+	case unpack (maybe NullValue (\ dat -> Value typ dat fmt) mbData) of
+		Just ret ->
+			ret <$ setColumn (col + 1)
 
--- | Get cell value.
-cellValue :: P.Row -> ColumnInfo -> ResultProcessor Value
-cellValue row (col, oid, fmt) = do
-	result <- ask
-	value <- lift (lift (P.getvalue' result row col))
-
-	case value of
-		Just dat -> pure (Value oid dat fmt)
-		Nothing  -> pure NullValue
-
--- | Get cell value.
-cellValue' :: P.Row -> P.Column -> ResultProcessor Value
-cellValue' row col = do
-	result <- ask
-	(oid, fmt, value) <- lift (lift ((,,) <$> P.ftype result col
-	                                      <*> P.fformat result col
-	                                      <*> P.getvalue' result row col))
-
-	case value of
-		Just dat -> pure (Value oid dat fmt)
-		Nothing  -> pure NullValue
-
-
--- | Unpack cell value.
-unpackCellValue :: (Column a) => P.Row -> ColumnInfo -> ResultProcessor a
-unpackCellValue row info =
-	withProxy Proxy
+		nothing ->
+			throwError (UnpackError row col typ fmt (describeColumn (makeProxy nothing)))
 	where
-		withProxy :: (Column a) => Proxy a -> ResultProcessor a
-		withProxy proxy = do
-			value <- cellValue row info
-			lift (ExceptT (make (describeColumn proxy) (unpack value)))
+		makeProxy :: Maybe a -> Proxy a
+		makeProxy _ = Proxy
 
-		(col, oid, fmt) = info
+-- | Process the entire result set.
+processResult :: P.Result -> ResultProcessor a -> ExceptT ResultError IO [a]
+processResult res (ResultProcessor proc) = do
+	rows <- liftIO (P.ntuples res)
+	cols <- liftIO (P.nfields res)
 
-		valueError desc =
-			pure (Left (ValueError row col oid fmt desc))
+	-- Iterate over each row number and run the row processor
+	forM [0 .. rows - 1] $ \ row ->
+		runReaderT (evalStateT proc 0) (res, row, cols)
 
-		make desc =
-			maybe (valueError desc) (pure . pure)
+-- | Process one row of the result set.
+processOneResult :: P.Result -> ResultProcessor a -> ExceptT ResultError IO (Maybe a)
+processOneResult res (ResultProcessor proc) = do
+	rows <- liftIO (P.ntuples res)
+	cols <- liftIO (P.nfields res)
 
--- | Result row
-class Result a where
-	-- | Extract rows from the given result.
-	resultProcessor :: ResultProcessor [a]
-
-instance Result () where
-	resultProcessor = foreachRow (const (pure ()))
-
-instance (Result a, Result b) => Result (a, b) where
-	resultProcessor =
-		zip <$> resultProcessor
-		    <*> resultProcessor
-
-instance (Result a, Result b, Result c) => Result (a, b, c) where
-	resultProcessor =
-		zip3 <$> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-
-instance (Result a, Result b, Result c, Result d) => Result (a, b, c, d) where
-	resultProcessor =
-		zip4 <$> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-
-instance (Result a, Result b, Result c, Result d, Result e) => Result (a, b, c, d, e) where
-	resultProcessor =
-		zip5 <$> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-
-instance (Result a, Result b, Result c, Result d, Result e, Result f) => Result (a, b, c, d, e, f) where
-	resultProcessor =
-		zip6 <$> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-
-instance (Result a, Result b, Result c, Result d, Result e, Result f, Result g) => Result (a, b, c, d, e, f, g) where
-	resultProcessor =
-		zip7 <$> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-		     <*> resultProcessor
-
--- | A row containing a list of all column values.
-newtype RawRow = RawRow [Value]
-	deriving (Show, Eq, Ord)
-
-instance Result RawRow where
-	resultProcessor = do
-		ncols <- numColumns
-		foreachRow $ \ row ->
-			RawRow <$> forM [0 .. ncols - 1] (cellValue' row)
+	if rows > 0 then
+		Just <$> runReaderT (evalStateT proc 0) (res, 0, cols)
+	else
+		pure Nothing
