a1d7852dcc0a4a377fb200f5675bacedca250ccf
Ole <ole@vprsm.de>
2016-07-18 02:08:14 +0200

    tests: Add test cases for Columns module

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,8 +1,29 @@
 module Main (main) where
 
-import Test.Hspec
-import Test.Database.PostgreSQL.Store
+import           Test.Hspec
+import           Test.Database.PostgreSQL.Store
+
+import           Data.String
+
+import qualified Database.PostgreSQL.LibPQ as P
+
+import           System.Environment
+
+connectMaybe :: String -> IO (Maybe P.Connection)
+connectMaybe info = do
+	con <- P.connectdb (fromString info)
+	status <- P.status con
+
+	pure $
+		if status == P.ConnectionOk then
+			Just con
+		else
+			Nothing
 
 -- | Test entry point
 main :: IO ()
-main = hspec allSpecs
+main = do
+	mbInfo <- lookupEnv "PGINFO"
+	mbCon <- maybe (pure Nothing) connectMaybe mbInfo
+
+	hspec (allSpecs mbCon)
--- tests/Test/Database/PostgreSQL/Store.hs
+++ tests/Test/Database/PostgreSQL/Store.hs
@@ -2,9 +2,14 @@ module Test.Database.PostgreSQL.Store (
 	allSpecs
 ) where
 
-import Test.Hspec
-import Test.Database.PostgreSQL.Store.Query
+import           Test.Hspec
 
-allSpecs :: Spec
-allSpecs =
+import           Test.Database.PostgreSQL.Store.Columns
+import           Test.Database.PostgreSQL.Store.Query
+
+import qualified Database.PostgreSQL.LibPQ as P
+
+allSpecs :: Maybe P.Connection -> Spec
+allSpecs mbCon = do
+	allColumnsSpecs mbCon
 	allQuerySpecs
