c6d3fe9228ff0c81de240b8292ee6f6cf2534f93
Ole <ole@vprsm.de>
2016-09-21 18:46:55 +0200

    Seperate module for table information classes

---
+++ src/Database/PostgreSQL/Store/Table/Class.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TemplateHaskell, BangPatterns, DeriveLift #-}
+
+-- |
+-- Module:     Database.PostgreSQL.Store.Table.Class
+-- Copyright:  (c) Ole Krüger 2016
+-- License:    BSD3
+-- Maintainer: Ole Krüger <ole@vprsm.de>
+module Database.PostgreSQL.Store.Table.Class (
+	TableInformation (..),
+	Table (..)
+) where
+
+import           Language.Haskell.TH
+import           Language.Haskell.TH.Syntax
+
+import qualified Data.ByteString as B
+
+-- | Table-related information about a type.
+data TableInformation = TableInformation {
+	tableName        :: !B.ByteString,
+	tableIdentColumn :: !B.ByteString,
+	tableColumns     :: ![B.ByteString]
+} deriving (Show, Eq, Ord)
+
+-- | Lift "ByteString".
+liftByteString :: B.ByteString -> Q Exp
+liftByteString bs =
+	[e| B.pack $(lift (B.unpack bs)) |]
+
+instance Lift TableInformation where
+	lift (TableInformation name identColumn columns) =
+		[e| TableInformation
+		        $(liftByteString name)
+		        $(liftByteString identColumn)
+		        $(listE (map liftByteString columns)) |]
+
+-- | Attach table information to a type.
+class Table a where
+	tableInfo :: proxy a -> TableInformation
