41045594ff85779452c8c7bf526cd25e2e557329
Ole <ole@vprsm.de>
2016-11-01 16:33:57 +0100

    First draft for 'Result' module

---
+++ src/Database/PostgreSQL/Store/Result.hs
@@ -0,0 +1,58 @@
+-- |
+-- Module:     Database.PostgreSQL.Store.Result
+-- Copyright:  (c) Ole Krüger 2016
+-- License:    BSD3
+-- Maintainer: Ole Krüger <ole@vprsm.de>
+module Database.PostgreSQL.Store.Result (
+	-- * Parser
+	RowParser,
+	RowParseError (..),
+	ResultParseError (..),
+
+	-- * Entity
+	ResultEntity (..),
+
+	-- * Processor
+	ResultProcessError,
+	processResultWith,
+	processResult
+) where
+
+import           Control.Monad.Except
+
+import           Database.PostgreSQL.Store.Result.Parser
+import           Database.PostgreSQL.Store.Result.Entity
+
+import qualified Database.PostgreSQL.LibPQ as P
+
+-- |
+data ResultProcessError
+	= ResultParseError ResultParseError
+	| UnsupportedStatus P.ExecStatus
+	deriving (Show, Eq)
+
+-- |
+validateResult :: P.Result -> ExceptT ResultProcessError IO ()
+validateResult result = do
+	status <- liftIO (P.resultStatus result)
+	case status of
+		P.EmptyQuery  -> pure ()
+		P.CommandOk   -> pure ()
+		P.TuplesOk    -> pure ()
+		P.SingleTuple -> pure ()
+		x             -> throwError (UnsupportedStatus x)
+
+		-- TODO: Handle 'BadResponse'
+		-- TODO: Handle 'NonfatalError'
+		-- TODO: Handle 'FatalError'
+
+-- |
+processResultWith :: P.Result -> RowParser a -> ExceptT ResultProcessError IO [a]
+processResultWith result parser = do
+	validateResult result
+	withExceptT ResultParseError (parseResult result parser)
+
+-- |
+processResult :: (ResultEntity a) => P.Result -> ExceptT ResultProcessError IO [a]
+processResult result =
+	processResultWith result parseEntity
