3a93a21496d7bd645c7667f2dc4391fa4143963c
Ole <ole@vprsm.de>
2016-11-07 00:07:51 +0100

    Add re-export module 'Query'

---
+++ src/Database/PostgreSQL/Store/Query.hs
@@ -0,0 +1,16 @@
+-- |
+-- Module:     Database.PostgreSQL.Store.Query
+-- Copyright:  (c) Ole Krüger 2016
+-- License:    BSD3
+-- Maintainer: Ole Krüger <ole@vprsm.de>
+--
+-- This module acts solely as a re-export unit.
+module Database.PostgreSQL.Store.Query (
+	module Database.PostgreSQL.Store.Query.Builder,
+	module Database.PostgreSQL.Store.Query.Entity,
+	module Database.PostgreSQL.Store.Query.TH
+) where
+
+import Database.PostgreSQL.Store.Query.Builder
+import Database.PostgreSQL.Store.Query.Entity
+import Database.PostgreSQL.Store.Query.TH
