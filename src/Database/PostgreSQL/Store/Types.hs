e0b595e2bf91cc2ac8a3f5dcd7ea77ed4a1a1969
Ole <ole@vprsm.de>
2017-02-01 18:23:13 +0100

    Move new types to 'Types' module, these will be accessed later

--- src/Database/PostgreSQL/Store/Types.hs
+++ src/Database/PostgreSQL/Store/Types.hs
@@ -13,32 +13,39 @@ module Database.PostgreSQL.Store.Types (
 	anyTypeValue,
 
 	Query (..),
+	PrepQuery (..),
 
-	PrepQuery (..)
+	Value2 (..),
+	Query2 (..),
+	PrepQuery2 (..)
 ) where
 
-import qualified Data.ByteString           as B
-import qualified Database.PostgreSQL.LibPQ as P
+import           Text.Show.Functions ()
+
+import           Data.Kind
+import qualified Data.ByteString as B
+
+import           Database.PostgreSQL.LibPQ (Oid (..), invalidOid)
 
 -- | Value of a cell in the result set
 newtype Value = Value { valueData :: B.ByteString }
 	deriving (Show, Eq, Ord)
 
--- | Value and type 'P.Oid' of a cell in the result set
-data TypedValue = TypedValue P.Oid (Maybe Value)
+-- | Value and type 'Oid' of a cell in the result set
+data TypedValue = TypedValue Oid (Maybe Value)
 	deriving (Show, Eq, Ord)
 
 -- | NULL value
 nullValue :: TypedValue
-nullValue = TypedValue P.invalidOid Nothing
+nullValue = TypedValue invalidOid Nothing
 
 -- | Placeholder value
 placeholderValue :: TypedValue
-placeholderValue = TypedValue (P.Oid maxBound) Nothing
+placeholderValue = TypedValue (Oid maxBound) Nothing
 
 -- | Value with any type
 anyTypeValue :: Value -> TypedValue
-anyTypeValue value = TypedValue P.invalidOid (Just value)
+anyTypeValue value = TypedValue invalidOid (Just value)
 
 -- | Query
 data Query a = Query {
@@ -51,3 +58,22 @@ data PrepQuery a = PrepQuery {
 	prepQueryName :: B.ByteString,
 	prepQuery     :: Query a
 } deriving (Show, Eq, Ord)
+
+-- | Replacement for 'TypedValue'
+data Value2
+	= Value2 Oid B.ByteString
+	| Null2
+	deriving (Show, Eq, Ord)
+
+-- | Replacement for 'Query'
+data Query2 = Query2 {
+	query2Statement :: B.ByteString,
+	query2Params    :: [Value2]
+} deriving (Show, Eq, Ord)
+
+-- | Replacement for 'PrepQuery'
+data PrepQuery2 a = PrepQuery2 {
+	prep2Name      :: B.ByteString,
+	prep2Statement :: B.ByteString,
+	prep2Params    :: a -> [Value2]
+} deriving (Show)
