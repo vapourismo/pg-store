967f27629e4a3d93021584681ae2617be99d20e9
Ole <ole@vprsm.de>
2017-02-05 18:31:05 +0100

    docs: Module 'RowParser'

--- src/Database/PostgreSQL/Store/RowParser.hs
+++ src/Database/PostgreSQL/Store/RowParser.hs
@@ -11,7 +11,7 @@
 -- License:    BSD3
 -- Maintainer: Ole Krüger <ole@vprsm.de>
 module Database.PostgreSQL.Store.RowParser (
-	-- * Row Parser
+	-- * Row parser
 	RowParser,
 	RowErrorLocation (..),
 	RowErrorDetail (..),
@@ -19,11 +19,14 @@ module Database.PostgreSQL.Store.RowParser (
 
 	processResultWith,
 
+	-- * Means of composition
 	finish,
 	cancel,
 	withRowParser,
 
-	parseValue
+	-- * Default parsers
+	parseValue,
+	parseByteString
 ) where
 
 import           GHC.TypeLits
@@ -31,6 +34,7 @@ import           GHC.TypeLits
 import           Control.Monad.Except
 
 import           Data.Proxy
+import qualified Data.ByteString as B
 
 import           Database.PostgreSQL.Store.Value
 
@@ -48,18 +52,22 @@ data RowErrorDetail
 		-- ^ A column value could not be parsed.
 	deriving (Show, Eq, Ord)
 
--- | An error that occured when parsing a row.
+-- | An error that occured when parsing a row
 data RowError = RowError RowErrorLocation RowErrorDetail
 	deriving (Show, Eq, Ord)
 
--- |
+-- | Shortcut for the internal monad transformer inside 'RowParser'
 type M = ExceptT RowError IO
 
--- |
+-- | Consumes @w@ columns of a result set row in order to produce an instance of @a@.
 newtype RowParser (w :: Nat) a = RowParser { runProcessor :: P.Result -> P.Row -> P.Column -> M a }
 
--- |
-processResultWith :: forall a n. (KnownNat n) => P.Result -> RowParser n a -> M [a]
+instance Functor (RowParser w) where
+	fmap f (RowParser action) =
+		RowParser (\ result row col -> f <$> action result row col)
+
+-- | Process the each row of the 'P.Result' with the given 'RowParser'.
+processResultWith :: forall a n. (KnownNat n) => P.Result -> RowParser n a -> ExceptT RowError IO [a]
 processResultWith result (RowParser run) = do
 	cols <- lift (P.nfields result)
 	when (cols < toEnum totalWidth) $
@@ -70,15 +78,16 @@ processResultWith result (RowParser run) = do
 	where
 		totalWidth = fromIntegral (natVal @n Proxy)
 
--- |
+-- | Terminate the parsing tree by returning the final result.
 finish :: a -> RowParser 0 a
 finish x = RowParser (\ _ _ _ -> pure x)
 
--- |
+-- | Terminate the parsing tree with an error.
 cancel :: RowErrorDetail -> RowParser 0 a
 cancel detail = RowParser (\ _ row col -> throwError (RowError (RowErrorLocation col row) detail))
 
--- |
+-- | Transform the result of another 'RowParser'. Similar to monadic bind. Also keeps track
+-- of how many columns are needed in total.
 withRowParser :: forall a v b w. (KnownNat v)
               => RowParser v a -> (a -> RowParser w b) -> RowParser (v + w) b
 withRowParser proc func =
@@ -86,7 +95,7 @@ withRowParser proc func =
 		x <- runProcessor proc result row col :: M a
 		runProcessor (func x) result row (col + fromIntegral (natVal @v Proxy))
 
--- |
+-- | Row parser which extracts a single column 'Value'.
 parseValue :: RowParser 1 Value
 parseValue =
 	RowParser $ \ result row col -> do
@@ -94,3 +103,12 @@ parseValue =
 		case mb of
 			Nothing  -> pure Null
 			Just cnt -> (\ typ -> Value typ cnt) <$> lift (P.ftype result col)
+
+-- | Extract the value of a cell. This parser will fail if value for that column is @NULL@.
+parseByteString :: RowParser 1 B.ByteString
+parseByteString =
+	RowParser $ \ result row col -> do
+		mb <- lift (P.getvalue' result row col)
+		case mb of
+			Just cnt -> pure cnt
+			Nothing  -> throwError (RowError (RowErrorLocation col row) (ColumnRejected Null))
